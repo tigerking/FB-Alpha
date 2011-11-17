3ed6f8dff8b83661b960519f7a0ed390a244880c
treble winner <barry@barryharris.me.uk>
2011-11-17 20:06:44 +0000

    Cleared the generated folder again....

--- src/depend/generated/build_details.h
+++
@@ -1,8 +0,0 @@
-#define BUILD_TIME 19:58:58
-#define BUILD_DATE Nov 17 2011
-#define BUILD_CHAR Unicode
-#define BUILD_CPU  X86
-#define BUILD_COMP Visual C++ 10.0
-#ifndef _MSC_VER
- #define _MSC_VER  1600
-#endif
