c92adba95c614b334b8760a54bf6a83e5a93e4c4
kev :) <kja@blueyonder.co.uk>
2011-11-20 15:53:29 +0000

    remove stuff from generated that shouldn't be there

--- src/depend/generated/build_details.h
+++
@@ -1,8 +0,0 @@
-#define BUILD_TIME 15:36:36
-#define BUILD_DATE Nov 20 2011
-#define BUILD_CHAR ANSI
-#define BUILD_CPU  X86
-#define BUILD_COMP Visual C++ 10.0
-#ifndef _MSC_VER
- #define _MSC_VER  1600
-#endif
